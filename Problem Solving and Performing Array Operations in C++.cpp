b868d05a0150ed3173fb43cdae5f45bef406d3fc
Shan Platon <shan_christian_platon@dlsl.edu.ph>
2024-09-03 18:01:50 +0800

    Lin and Lan house type

--- Problem Solving and Performing Array Operations in C++.cpp
+++ Problem Solving and Performing Array Operations in C++.cpp
@@ -4,39 +4,54 @@
 #include <limits> // for numeric_limits
 using namespace std;
 
-	int House[5], bedSize[5], numHouse = 0, counter, numBathroom, numBedroom;
+	int bedSize, numHouse, i, numBathroom, numBedroom;
     int paintTime, paintWorker, floorTime, floorWorker, kitchenSize;
-    float paintCost, flooringCost, paintTotal[5], paintLaborCost, floorLaborCost;
+    float paintCost, flooringCost, paintLaborCost, floorLaborCost, paintTotal[5], floorTotal[5], pLaborTotal[5], fLaborTotal[5], daysTotal[5];
 	string houseType;
 
 
 void displayTotal(){
 	
-	for(counter = 0; counter<numHouse; counter++){
+	for(int i = 0; i<numHouse; i++){		
 		
-	cout<<"\nTotal Cost for House #"<<counter+1<<endl;
-	cout<<"Painting Cost: P"<<paintCost<<endl;
-	cout<<"Flooring Cost: P"<<flooringCost<<endl;
-	cout<<"Paint Labor Cost: P"<<paintLaborCost<<endl;
-	cout<<"Flooring Labor Cost: P"<<floorLaborCost<<endl;
-	cout<<"Total Overall Cost: P"<<paintCost + flooringCost + paintLaborCost + floorLaborCost<<endl;
-	cout<<"Total Days: "<<paintTime + floorTime<<" days"<<endl;
+	cout<<"\nTotal Cost for House #"<<i+1<<endl;
+	cout<<"Painting Cost: P"<<paintTotal[i]<<endl;
+	cout<<"Flooring Cost: P"<<floorTotal[i]<<endl;
+	cout<<"Paint Labor Cost: P"<<pLaborTotal[i]<<endl;
+	cout<<"Flooring Labor Cost: P"<<fLaborTotal[i]<<endl;
+	cout<<"Total Overall Cost: P"<<paintTotal[i] + floorTotal[i] + pLaborTotal[i] + fLaborTotal[i]<<endl;
+	cout<<"Total Days: "<<daysTotal[i]<<" days"<<endl;
 
 }
-	
-	system("PAUSE");
-	
-	// Resets the value for the next iterations
-	paintCost = 0;
-	flooringCost = 0;
-	paintLaborCost = 0;
-	floorLaborCost = 0;
+
+	// Adds all the value for the projected expense	
+//	for(int i = 0; i<numHouse; i++){
+//		
+//		cout<<"GAYYYY";
+//		paintTotal[i] += paintTotal[i];
+//		floorTotal[i] += floorTotal[i];
+//		pLaborTotal[i] += pLaborTotal[i];
+//		fLaborTotal[i] += fLaborTotal[i];
+//		
+//		if(i>=numHouse){
+//			
+//		cout<<"\nOverall Projected Expenses:";
+//		cout<<"Total Cost for House #"<<counter+1<<endl;
+//		cout<<"Total Painting Cost: P"<<paintTotal[i]<<endl;
+//		cout<<"Total Flooring Cost: P"<<floorTotal[i]<<endl;
+//		cout<<"Total Labor Cost: P"<<pLaborTotal[i] + fLaborTotal[i]<<endl;
+//		cout<<"Total Expenses: P"<<paintTotal[i] + floorTotal[counter] + pLaborTotal[i] + fLaborTotal[i]<<endl;
+//		cout<<"Total Number of Days: "<<daysTotal[i]<<" days"<<endl;	
+//		
+//		}
+//		
+//	}	
 	
 }
 
-void LinHouse(){
-	
-	do{	
+void House(){
+		
+	do{
 		
 	cout<<"\nEnter the number of bedrooms: ";
 	cin>>numBedroom;
@@ -44,30 +59,38 @@ void LinHouse(){
 	if(numBedroom>2 && numBathroom<5){
 		flooringCost += (numBedroom-2)*8500;
 	}
-		
+			
+	if(houseType == "lin"){  // sets the limits for lin
+	
 	if(numBedroom<1 || numBedroom>4){
 		cout<<"\nOnly 1-4 bedroom capacity";
 	}
+}
+	if(houseType == "lan"){  // sets the limits for lan
 	
+	if(numBedroom<1 || numBedroom>5){
+		cout<<"\nOnly 1-5 bedroom capacity";
+	}
+}
+
 }while(numBedroom<1 || numBedroom>4);
-	
 	
 	for(int i=0; i<numBedroom; i++){
 		
 		cout<<"Enter size of bedroom "<<i+1<<" in sqm: ";
-		cin>>bedSize[i];
+		cin>>bedSize;
 		
 		// Calculates the value of the painting cost
-		if(bedSize[i]<30){
-			paintCost += bedSize[i]*125;
+		if(bedSize<30){
+			paintCost += bedSize*125;
 		}
 		
-		else if(bedSize[i]<41){
-			paintCost += bedSize[i]*175.25;
+		else if(bedSize<41){
+			paintCost += bedSize*175.25;
 		}
 		
 		else{
-			paintCost += bedSize[i]*205.50;
+			paintCost += bedSize*205.50;
 		}
 	}
 
@@ -91,6 +114,8 @@ void LinHouse(){
 	
 	// Adds value to paintCost for total cost
 	paintCost += numBathroom*6550;
+
+	if(houseType == "lin"){
 
 	do {
         cout<<"Enter number of workers for painting: ";
@@ -136,9 +161,58 @@ void LinHouse(){
         }
     } while (true); // Continue until a valid input is received
     
-    paintLaborCost += (paintTime*650)*paintWorker; // Calculates the labor cost for painting
+    cout<<"\n";
+    
+}
+	
+	if(houseType == "lan"){
+	
+		do {
+        cout<<"Enter number of workers for painting: ";
+		cin>>paintWorker;
+        cin.ignore();
+
+        if (cin.fail()) {
+            // Clear the error flag
+            cin.clear();
+
+            // Ignore the invalid input
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            cout << "Invalid input. Please enter a number between 10 and 15." << endl;
+            
+        } else if (paintWorker < 10 || paintWorker > 15) {
+            cout << "Incorrect input. Please enter a number between 10 and 15." << endl;
+            
+        } else {
+            break; // Valid input, break out of the loop
+        }
+    } while (true); // Continue until a valid input is received
+
+	do {
+        cout<<"Enter number of days for painting: ";
+        cin>>paintTime;
+        cin.ignore();
+
+        if (cin.fail()) {
+            // Clear the error flag
+            cin.clear();
+
+            // Ignore the invalid input
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            cout << "Invalid input. Please enter a number between 7 and 14." << endl;
+            
+        } else if (paintTime < 7 || paintTime > 14) {
+            cout << "Incorrect input. Please enter a number between 7 and 14." << endl;
+            
+        } else {
+            break; // Valid input, break out of the loop
+        }
+    } while (true); // Continue until a valid input is received
     
     cout<<"\n";
+}
     
     do {
         cout<<"Enter number of workers for flooring: ";
@@ -163,7 +237,7 @@ void LinHouse(){
     } while (true); // Continue until a valid input is received
     
     do {
-        cout<<"Enter number of days for painting: ";
+        cout<<"Enter number of days for flooring: ";
         cin>>floorTime;
         cin.ignore();
 
@@ -183,8 +257,26 @@ void LinHouse(){
             break; // Valid input, break out of the loop
         }
     } while (true); // Continue until a valid input is received
+	
+	
 
+    
+    paintLaborCost += (paintTime*650)*paintWorker; // Calculates the labor cost for painting
 	floorLaborCost += (floorTime*550)*floorWorker; // Calculates the labor cost for flooring
+	
+	// Puts the cost of each house in a holder
+	paintTotal[i] = paintCost;
+	floorTotal[i]	= flooringCost;
+	pLaborTotal[i] = paintLaborCost;
+	fLaborTotal[i] = floorLaborCost;
+	daysTotal[i] = paintTime + floorTime;
+	
+	
+	// Resets the value for the next iterations
+	paintCost = 0;
+	flooringCost = 0;
+	paintLaborCost = 0;
+	floorLaborCost = 0;
 	
 }
 
@@ -215,7 +307,7 @@ int main(){
         }
     } while (true); // Continue until a valid input is received
 	
-	for(counter = 0; counter<numHouse; counter++){
+	for(i = 0; i<numHouse; i++){
 		
 	// Handles the inputs for the house type
 	do{
@@ -230,21 +322,20 @@ int main(){
     if(houseType == "lin"){
     	
     	flooringCost += 123450; //Flooring cost initialized to set value for Lin
-		LinHouse(); // Calls the function
+		House(); // Calls the function
 		
 	}
 	
 	else if(houseType == "lan"){
 		
 		flooringCost += 180500; //Flooring cost initialized to set value for Lan
-//		LanHouse(); // Calls the function
+		House(); // Calls the function
 	}
 	
 	else{
 		cout<<"Invalid input."<<endl;
 	}
-        
-      	
+           	
 }while(!(houseType=="lin" || houseType=="lan"));
 	
 }
